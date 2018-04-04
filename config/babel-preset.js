0b68970e2b64bdced64b5927afc19d810d3fc012
Alexander <alex.nanberg@hotmail.com>
2018-04-04 19:30:06 +0200

    Fix babel 7 compability

--- config/babel-preset.js
+++ config/babel-preset.js
@@ -1,4 +1,4 @@
-module.exports = {
+module.exports = () => ({
   presets: [
     [
       '@babel/env',
@@ -16,4 +16,4 @@ module.exports = {
     'react-hot-loader/babel',
     'styled-components',
   ],
-}
+})
