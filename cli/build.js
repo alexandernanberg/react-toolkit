b7a6e339deca8f6e851fbf818b1bfc3ac9ccfcf9
Alexander Nanberg <alexander.nanberg@strateg.se>
2019-05-14 13:21:52 +0200

    chore: move build scripts to separate files

--- cli/build.js
+++ cli/build.js
@@ -1,17 +1,5 @@
 const arg = require('arg')
-const chalk = require('chalk')
-const fs = require('fs-extra')
-const webpack = require('webpack')
-const checkRequiredFiles = require('react-dev-utils/checkRequiredFiles')
-const createWebpackConfig = require('../config/webpack.config')
-const paths = require('../config/paths')
-
-function copyPublicFolder() {
-  fs.copySync(paths.appPublic, paths.appBuild, {
-    dereference: true,
-    filter: file => file !== paths.appHtml,
-  })
-}
+const compile = require('../lib/build')
 
 module.exports = async function build(argv) {
   const args = arg(
@@ -39,19 +27,5 @@ Options
     process.exit(0)
   }
 
-  if (!checkRequiredFiles([paths.appHtml, paths.appIndexJs])) {
-    process.exit(1)
-  }
-
-  console.log(chalk.blue('Creating a optimized production bundle...'))
-
-  fs.emptyDirSync(paths.appBuild)
-  copyPublicFolder()
-
-  const config = createWebpackConfig()
-  const compiler = webpack(config)
-
-  compiler.run((err, stats) => {
-    // console.log(stats)
-  })
+  compile()
 }
--- cli/dev.js
+++ cli/dev.js
@@ -1,12 +1,5 @@
 const arg = require('arg')
-const chalk = require('chalk')
-const webpack = require('webpack')
-const WebpackDevServer = require('webpack-dev-server')
-const { findAPortNotInUse } = require('portscanner')
-const checkRequiredFiles = require('react-dev-utils/checkRequiredFiles')
-const paths = require('../config/paths')
-const createWebpackConfig = require('../config/webpack.config')
-const createDevServerConfig = require('../config/webpackDevServer.config')
+const { startDevelopmentServer } = require('../lib/startDevelopmentServer')
 
 const HOST = '0.0.0.0'
 const DEFAULT_PORT = 3000
@@ -43,27 +36,8 @@ Options
     process.exit(0)
   }
 
-  if (!checkRequiredFiles([paths.appHtml, paths.appIndexJs])) {
-    process.exit(1)
-  }
+  const host = args['--hostname'] || HOST
+  const port = args['--port'] || DEFAULT_PORT
 
-  const host = arg['--hostname'] || HOST
-  const port = await findAPortNotInUse(
-    args['--port'] || DEFAULT_PORT,
-    null,
-    host
-  )
-
-  const compiler = createWebpackConfig({ host, port })
-  const serverConfig = createDevServerConfig({ host })
-  const devServer = new WebpackDevServer(webpack(compiler), serverConfig)
-
-  devServer.listen(port, host, err => {
-    if (err) {
-      console.log(err)
-      return
-    }
-
-    console.log(chalk.blue('Starting the development server...\n'))
-  })
+  startDevelopmentServer({ host, port })
 }
